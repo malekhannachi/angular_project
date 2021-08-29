869e304991497f47326df39d9d7d06295810cd21
malekhannachi <malek.hannachi05@gmail.com>
2021-08-29 16:50:06 +0100

    password and repassaword

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -22,12 +22,6 @@ export class RegisterComponent implements OnInit {
         Validators.pattern("[a-z .'-]+"),
         Validators.minLength(2)
       ]),
-      phone: new FormControl('',[
-        Validators.required,
-        Validators.pattern("[0-9]+"),
-        Validators.minLength(8),
-        Validators.maxLength(13)
-      ]),
       email: new FormControl('',[
         Validators.required,
         Validators.email
@@ -46,7 +40,6 @@ export class RegisterComponent implements OnInit {
 
   get firstname() { return this.registerForm.get('firstname') }
   get lastname() { return this.registerForm.get('lastname') }
-  get phone() { return this.registerForm.get('phone') }
   get email() { return this.registerForm.get('email') }
   get password() { return this.registerForm.get('password') }
   get repassword() { return this.registerForm.get('repassword') }
