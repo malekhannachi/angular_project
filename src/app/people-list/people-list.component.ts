4e9e165972a38e6842799d49849ac3794ab4ddb4
malekhannachi <malek.hannachi05@gmail.com>
2021-08-29 22:49:27 +0100

    add user

--- src/app/people-list/people-list.component.ts
+++ src/app/people-list/people-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-people-list',
@@ -6,25 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./people-list.component.css']
 })
 export class PeopleListComponent implements OnInit {
-peopleList=[
-  {id :1,name :"malek",lastname:"hannachi",phone :"20 741 701" },
-  {id :2,name :"salma",lastname:"salouma",phone :"90 457 701" },
-  {id :3,name :"ahmed",lastname:"ahmed50",phone :"50 545 221" },
-  {id :4,name :"noura",lastname:"noura123",phone :"94 545 701" }
+peopleList:any[]=[
 ]
 
 myConditon=false;
 
-delete(person: { id: number; name: string; lastname: string; phone: string; }) {
+delete(person: { _id: String; name: string; lastname: string; phone: string; }) {
   let index = this.peopleList.indexOf(person);
   this.peopleList.splice(index, 1);
+  this.userService.deleteUser(person._id).subscribe(
+    res=>{
+      console.log(res);
+    },
+    err =>{
+      console.log(err);
+    }
+  )
 
 }
-  constructor() { }
+  constructor(private userService:UserService) { }
 
   
 
   ngOnInit(): void {
+    this.userService.getAllUsers().subscribe(
+      result=>{
+        this.peopleList = result
+      },
+      error=>{
+        console.log(error);
+      }
+    )
   }
-
+ 
 }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,14 +1,27 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { User } from './user';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+private getAllUsersUrl ="https://backend-people-crud-app.herokuapp.com/users";
+private deleteUserUrl = "https://backend-people-crud-app.herokuapp.com/users/";
+private addUserUrl = "https://backend-people-crud-app.herokuapp.com/users/add";
 
   constructor(private http:HttpClient) {  }
 
+
   getAllUsers(){
-    let data = this.http.get<any>("https://jsonplaceholder.typicode.com/users");
-    return data;
+   return this.http.get<any>(this.getAllUsersUrl);
   }
+  deleteUser(id:String){
+    return this.http.delete<any>(this.deleteUserUrl+id)
+  }
+
+  addUser(user:User){
+    return this.http.post<any>(this.addUserUrl,user);
+  }
+
+
 }
